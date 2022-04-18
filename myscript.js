3d1e41fbefba4be1d95e013dc412e7b9f39fcd7d
muhammad.naveed@brainxtech.com <muhammad.naveed@brainxtech.com>
2022-04-18 14:29:26 +0500

    Evaluation function added

--- myscript.js
+++ myscript.js
@@ -1,12 +1,16 @@
+const quiz = {
+    Q1: {stem: "Which of the following is used for style? ", choices:["js", "css", "html", "php"]},
+    Q2: {stem: "Capital of Pakistan? ", choices:["Karachi", "Lahore", "Islamabad", "Peshawar"]},
+    Q3: {stem: "National Poet? ", choices:["Quid-e-Azam", "Iqbal", "Bhutto", "Imran"]},
+    Q4: {stem: "Prime Minister of Pakistan? ", choices:["Nawaz", "Shahbaz", "Imran", "Bilawal"]},
+    Q5: {stem: "Capital of India? ", choices:["Mumbai", "Delhi", "Gowa", "Luckhnow"]}
+}
+
+const answers = ['css',"Islamabad","Iqbal","Shahbaz","Delhi"];
+
+
 function loadQuiz(){
     let x = "";
-    const quiz = {
-        Q1: {stem: "Which of the following is used for style? ", choices:["js", "css", "html", "php"]},
-        Q2: {stem: "Capital of Pakistan? ", choices:["Karachi", "Lahore", "Islamabad", "Peshawar"]},
-        Q3: {stem: "National Poet? ", choices:["Quid-e-Azam", "Alama Iqbal", "Bhutto", "Imran"]},
-        Q4: {stem: "Prime Minister of Pakistan? ", choices:["Nawaz", "Shahbaz", "Imran", "Bilawal"]},
-        Q5: {stem: "Capital of India? ", choices:["Mumbai", "New Delhi", "Gowa", "Luckhnow"]}
-    }
     x += "<form>"
     for(let i in quiz){
         x += "<div> <p>" + i + " " + quiz[i].stem +"</p>";
@@ -15,9 +19,48 @@ function loadQuiz(){
             x += "<label for=" + ch +">" + ch + "</label><br>";
         }
         
-        x += "</div>";      
+        x += "</div><br>";      
     }
-    x += "<br>" + "<input type = button value = Evaluate>" + "</form>";
+    x += "<br>" + "<input type = button value = Evaluate id = btnEval>" + "</form>";
 
     document.getElementById("id01").innerHTML = x;
+
+    //creating result div
+    const para = document.createElement("p");
+
 }
+
+// Event delegation: Event Listener is added to one parent which analyze bubbled events to find
+// a match on child events
+
+document.addEventListener('click',function(e){
+    let predictions = [];
+    let score = 0;
+    if(e.target && e.target.id== 'btnEval'){
+        // do something
+        for(let q in quiz){
+            let elements = document.getElementsByName(q);
+            let flag = false;
+            for(let elem of elements){
+                if (elem.checked) {
+                    predictions.push(elem.value);
+                    flag = true;
+                    break;  
+                } 
+            }
+            if(!flag){
+                predictions.push("Not-Selected");
+            }
+        }
+// calculating score
+        for(let i=0; i < answers.length; i++){
+            if(answers[i] === predictions[i]){
+                score++;
+            }
+        }
+      
+        
+        document.getElementById("result").innerHTML = "Your score is " + score + " out of 5";
+     }
+ })
+
